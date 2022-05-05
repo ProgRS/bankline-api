b3b2aed1c19b394821de5e77d001e8b48fb9a6bf
Luis Fernando Trindade <lfernandotc@hotmail.com>
2022-05-05 18:44:16 -0300

    Versao final do projeto.

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -29,6 +29,9 @@ public class Movimentacao {
 		@Enumerated(EnumType.STRING)
 		private MovimentacaoTipo tipo;
 		
+		@Column(name = "id_conta")
+		private Integer idConta;
+		
 		
 		public Integer getId() {
 			return id;
@@ -60,6 +63,14 @@ public class Movimentacao {
 		public void setTipo(MovimentacaoTipo tipo) {
 			this.tipo = tipo;
 		}
+		public Integer getIdConta() {
+			return idConta;
+		}
+		public void setIdConta(Integer idConta) {
+			this.idConta = idConta;
+		}
+		
+		
 		
 		
 		
